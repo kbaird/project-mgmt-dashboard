abd974a5ec6b47aa8c3096f47444acb3e45f1c7c
Kevin Baird <kevincbaird@gmail.com>
2023-08-16 11:37:42 -0700

    Get rubocop green

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# Generic Helper
 module ApplicationHelper
 end
--- db/migrate/20230816181646_create_project_managers.rb
+++ db/migrate/20230816181646_create_project_managers.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# bundle exec rails g model ProjectManager name:string email:string password_digest:string
 class CreateProjectManagers < ActiveRecord::Migration[7.0]
   def change
     create_table :project_managers, id: :uuid do |t|
