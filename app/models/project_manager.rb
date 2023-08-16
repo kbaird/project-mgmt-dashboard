91712bf597aa4353f6416c0d887b3e217f01e90e
Kevin Baird <kevincbaird@gmail.com>
2023-08-16 11:26:29 -0700

    Add has_secure_password to ProjectManager model w/specs

--- app/models/project_manager.rb
+++ app/models/project_manager.rb
@@ -1,2 +1,3 @@
 class ProjectManager < ApplicationRecord
+  has_secure_password
 end
--- spec/models/project_manager_spec.rb
+++ spec/models/project_manager_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe ProjectManager, type: :model do
     it { expect(project_manager).to respond_to(:email) }
     it { expect(project_manager).to respond_to(:password_digest) }
   end
+
+  describe 'password' do
+    it 'is hashed with bcrypt' do
+      pm = described_class.create(name: 'Sample PM', email: 'sample_pm@example.com', password: 'foobar')
+      expect(pm.password_digest).not_to eq('foobar')
+      expect(pm.authenticate('foobar')).to be(pm)
+      expect(pm.authenticate('wrong_password')).to be(false)
+    end
+  end
 end
